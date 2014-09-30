780fdf29bd185fd6546a424a86d690113b38a99f
44075 <44075@longroad.ac.uk>
2014-09-30 12:18:51 +0100

    pay rate v1

---
+++ hours and pay rate.py
@@ -0,0 +1,11 @@
+#Callum Brown
+#30-09-14
+#Hours worked and pay rate
+
+hours = int(input("Please enter the amount of hours you have worked the week: "))
+pay_rate = int(input("Please enter your hourly rate of pay: "))
+
+gross_pay = (hours*pay_rate)
+
+print("Your gross pay is {0} pounds ".format(hours*pay_rate))
+
