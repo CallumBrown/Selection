277babdd3e40989245b2b817be7e25fd40029a42
44075 <44075@longroad.ac.uk>
2014-09-30 12:48:24 +0100

    grade boundries v 1

---
+++ grade boundries.py
@@ -0,0 +1,19 @@
+#Callum Brown
+#30-09-14
+#Grade Boundries
+
+mark = int(input("PLease enter the mark you got in an exam out of 100: "))
+
+if mark > 81:
+    print ("Grade A")
+elif mark > 71:
+    print ("Grade B")
+elif mark > 61:
+    print ("Grade C")
+elif mark > 51:
+    print ("Grade D")
+elif mark > 41:
+    print("Grade E")
+else:
+    print ("Grade U")
+    
