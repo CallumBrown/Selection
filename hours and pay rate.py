16f8d8f17b08a915890094d999de388e1dd59e81
44075 <44075@longroad.ac.uk>
2014-10-03 13:48:29 +0100

    pay rate v2

--- hours and pay rate.py
+++ hours and pay rate.py
@@ -4,8 +4,15 @@
 
 hours = int(input("Please enter the amount of hours you have worked the week: "))
 pay_rate = int(input("Please enter your hourly rate of pay: "))
-
-gross_pay = (hours*pay_rate)
-
-print("Your gross pay is {0} pounds ".format(hours*pay_rate))
+no_overtime = hours*pay_rate
+if hours < 61:
+    if hours > 40:
+        over_time = hours - 40
+        over_pay = over_time*1.5
+        pay = (hours - over_time)*pay_rate
+        overall_pay = over_pay+pay
+        print("Your overall pay is £{0}!".format(overall_pay))
+    else:
+        print("Your overall pay is £{0}!".format(no_overtime))
+        
 
